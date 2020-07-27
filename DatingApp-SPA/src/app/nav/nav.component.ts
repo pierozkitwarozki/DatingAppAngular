4cb9fe15f49f6a6d864c9a207b0478bd90e2b0d5
pierozkitwarozki <bartekhenry14@gmail.com>
2020-07-27 15:37:54 +0200

    End of section 7 'routing'

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,10 +13,11 @@ export class NavComponent implements OnInit {
 
   constructor(
     public authService: AuthService,
-    private alertifyService: AlertifyService
+    private alertifyService: AlertifyService,
+    private router: Router
   ) {}
 
-  ngOnInit() {} 
+  ngOnInit() {}
 
   login() {
     this.authService.login(this.model).subscribe(
@@ -24,6 +26,8 @@ export class NavComponent implements OnInit {
       },
       (error) => {
         this.alertifyService.error(error);
+      }, () => {
+        this.router.navigate(['/members']);
       }
     );
   }
@@ -35,5 +39,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertifyService.message('Logged out');
+    this.router.navigate(['/home']);
   }
 }
